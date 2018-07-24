756907174eb39e321b6977e59f7a5fab7bd145a5
Achilles-10 <1029991674@qq.com>
2018-07-24 17:32:33 +0800

    error

--- awesome-python3-webapp/www/models.py
+++ awesome-python3-webapp/www/models.py
@@ -22,7 +22,7 @@ class Blog(Model):
 	user_name=StringField(ddl='varchar(50)')
 	user_image=StringField(ddl='varchar(500)')
 	name=StringField(ddl='varchar(50)')
-	summary=StringField(ddl='varchar(50)')
+	summary=StringField(ddl='varchar(200)')
 	content=TextField()
 	created_at=FloatField(default=time.time)
 
